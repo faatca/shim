8f18c31dd35a207b07abef801b682fe4115f2cc5
Aaron Milner <aaron.milner@gmail.com>
2020-12-28 08:57:56 -0700

    Adds list and show commands to shims.py

--- shims.py
+++ shims.py
@@ -9,7 +9,7 @@ log = logging.getLogger(__name__)
 
 
 def main():
-    parser = argparse.ArgumentParser(description="Shim management tools")
+    parser = argparse.ArgumentParser(description="Shim management tool")
     subparsers = parser.add_subparsers(dest="command", help="the command to execute")
     subparsers.required = True
 
@@ -27,8 +27,25 @@ def main():
     add_parser.add_argument("args", nargs=argparse.REMAINDER)
     add_parser.set_defaults(func=do_add)
 
+    list_parser = subparsers.add_parser(
+        "list",
+        description="Lists the added shims",
+        help="lists the added shims",
+        parents=[parent_parser],
+    )
+    list_parser.set_defaults(func=do_list)
+
+    show_parser = subparsers.add_parser(
+        "show",
+        description="Shows information about a shim",
+        help="shows information about a shim",
+        parents=[parent_parser],
+    )
+    show_parser.add_argument("name")
+    show_parser.set_defaults(func=do_show)
+
     remove_parser = subparsers.add_parser(
-        "remove", description="Removes a shim", help="deletes a shim", parents=[parent_parser],
+        "remove", description="Removes a shim", help="deletes a shim", parents=[parent_parser]
     )
     remove_parser.add_argument("name")
     remove_parser.set_defaults(func=do_remove)
@@ -66,6 +83,18 @@ def do_add(args):
     shutil.copy(shim_source_path, shim_exec_path)
 
 
+def do_list(args):
+    for path in SHIM_DIR.glob("*.shim"):
+        print(path.name[: -len(".shim")])
+
+
+def do_show(args):
+    shim_data_path = SHIM_DIR / (args.name + ".shim")
+    if not shim_data_path.is_file():
+        raise Exception(f"Unknown shim: {args.name}")
+    print(shim_data_path.read_text())
+
+
 def do_remove(args):
     shim_exec_path = SHIM_DIR / (args.name + ".exe")
     shim_data_path = SHIM_DIR / (args.name + ".shim")
@@ -76,7 +105,7 @@ def do_remove(args):
     if not shim_data_path.is_file():
         raise Exception(f"Not found: {shim_data_path}")
 
-    log.debug(f"Removing executable: {shim_exec_path}", )
+    log.debug(f"Removing executable: {shim_exec_path}")
     shim_exec_path.unlink()
     log.debug(f"Removing data file: {shim_data_path}")
     shim_data_path.unlink()
