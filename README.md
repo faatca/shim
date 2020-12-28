f76786f79423760dd2d8919371b8945d57f0465e
Aaron Milner <aaron.milner@gmail.com>
2020-12-28 08:51:54 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+Shim tool
+=========
+
+This code was largely adapted from the shim tool from scoop.
+
+https://github.com/ScoopInstaller/Shim
+
+I also created a shims.py script to easily add and remove the shims from the command line.
---
+++ shims.py
@@ -0,0 +1,88 @@
+import argparse
+import logging
+from pathlib import Path
+import shutil
+from subprocess import list2cmdline
+import sys
+
+log = logging.getLogger(__name__)
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Shim management tools")
+    subparsers = parser.add_subparsers(dest="command", help="the command to execute")
+    subparsers.required = True
+
+    parent_parser = argparse.ArgumentParser(add_help=False)
+    parent_parser.add_argument("-v", "--verbose", action="store_true", help="shows debug messages")
+
+    add_parser = subparsers.add_parser(
+        "add",
+        description="Creates a new shim",
+        help="creates a new shim",
+        parents=[parent_parser],
+    )
+    add_parser.add_argument("name")
+    add_parser.add_argument("path", type=Path)
+    add_parser.add_argument("args", nargs=argparse.REMAINDER)
+    add_parser.set_defaults(func=do_add)
+
+    remove_parser = subparsers.add_parser(
+        "remove", description="Removes a shim", help="deletes a shim", parents=[parent_parser],
+    )
+    remove_parser.add_argument("name")
+    remove_parser.set_defaults(func=do_remove)
+
+    args = parser.parse_args()
+
+    logging.basicConfig(
+        level=logging.DEBUG if args.verbose else logging.INFO,
+        format="%(asctime)s %(levelname)-8s %(name)s %(message)s",
+    )
+
+    try:
+        args.func(args)
+    except Exception:
+        log.exception("Unexpected error encountered.")
+        sys.exit(2)
+
+
+def do_add(args):
+    log.debug("Checking that path exists")
+    if not args.path.is_file():
+        raise FileNotFoundError(f"File does not exist: {args.path.absolute()}")
+
+    shim_source_path = Path(__file__).parent / "Shim.exe"
+
+    shim_exec_path = SHIM_DIR / (args.name + ".exe")
+    shim_data_path = SHIM_DIR / (args.name + ".shim")
+
+    log.debug(f"Adding shim data file: {shim_data_path}")
+    with shim_data_path.open("w") as f:
+        print("path =", args.path.absolute(), file=f)
+        print("args =", list2cmdline(args.args), file=f)
+
+    log.debug(f"Copying shim executable to: {shim_exec_path}")
+    shutil.copy(shim_source_path, shim_exec_path)
+
+
+def do_remove(args):
+    shim_exec_path = SHIM_DIR / (args.name + ".exe")
+    shim_data_path = SHIM_DIR / (args.name + ".shim")
+
+    log.debug("Checking that files exist")
+    if not shim_exec_path.is_file():
+        raise Exception(f"Not found: {shim_exec_path}")
+    if not shim_data_path.is_file():
+        raise Exception(f"Not found: {shim_data_path}")
+
+    log.debug(f"Removing executable: {shim_exec_path}", )
+    shim_exec_path.unlink()
+    log.debug(f"Removing data file: {shim_data_path}")
+    shim_data_path.unlink()
+
+
+SHIM_DIR = Path(r"C:\CustomTools\Shortcuts")
+
+if __name__ == "__main__":
+    main()
